8dab31a6d42458de5bb6131f82f281600e8a724d
Crowdbotics <team@crowdbotics.com>
2020-07-15 03:06:01 +0000

    Initial commit.

---
+++ backend/tysgiftedhands_18910/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for tysgiftedhands_18910 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tysgiftedhands_18910.settings')
+
+application = get_wsgi_application()
